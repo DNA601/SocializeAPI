c5a45e52cd9d4a83e46ee3ee1a971b21479868c2
DNA601 <dnavarrete6001@gmail.com>
2022-08-17 14:12:18 -0700

    will get all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,20 @@
 const { Thought, User } = require('../models');
 
 const userControl = {
-
+    getUsers(req, res) { // function will get all of the Users. 
+        User.find({})
+            .populate({
+                path: "friends",
+                select: "-__v",
+            })
+            .select("-__v")
+            .sort({ _id: -1 })
+            .then((userData) => res.json(userData))
+            .catch((err) => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
 
 };
 module.exports = userControl;
