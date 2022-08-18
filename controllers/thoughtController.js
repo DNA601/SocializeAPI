3f0bb9386e059e92ff75526328d14fe0ac204477
DNA601 <dnavarrete6001@gmail.com>
2022-08-18 10:13:41 -0700

    adding reaction and updating a thought'

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -19,35 +19,71 @@ const thoughtControl = {
     },
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    getThoughts(req, res) { //will get user thoughts
+        Thought.find({})
+            .populate({
+                path: 'reactions',
+                select: "-__v",
+            })
+            .select("-__v")
+            .sort({ _id: -1 })
+            .then((thoughtData) => res.json(thoughtData))
+            .catch((err) => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
 
-
-
-
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    getThoughtId({ params }, res) { //getting a thought by an id.
+        Thought.findOne({ _id: params.id })
+            .populate({
+                path: "reactions",
+                select: "-__v",
+            })
+            .select("-__v")
+            .then((thoughtData) => {
+                if (!thoughtData) {
+                    return res.status(404).json({ message: 'id does not exist. no thought found' });
+                }
+                res.json(thoughtData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-
+    updateThought({ params, body }, res) { // this will be updating thoughts. 
+        Thought.findOneAndUpdate({ _id: params.id }, body, {
+                new: true,
+                runValidators: true,
+            })
+            .then((thoughtData) => {
+                if (!thoughtData) {
+                    res.status(404).json({ message: 'thought id not found' });
+                    return;
+                }
+                res.json(thoughtData);
+            })
+            .catch((err) => res.json(err));
+    },
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-
-
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
+    reaction({ params, body }, res) { // this will be adding reactions
+        Thought.findOneAndUpdate({ _id: params.thoughtId }, { $addToSet: { reactions: body } }, { new: true, runValidators: true })
+            .then((thoughtData) => {
+                if (!thoughtData) {
+                    res.status(404).json({ message: 'thought id not found' });
+                    return;
+                }
+                res.json(thoughtData);
+            })
+            .catch((err) => res.json(err));
+    },
 
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
