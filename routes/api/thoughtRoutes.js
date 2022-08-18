e53ecfefa1e104a1bc46af7db4724e9ccd81943d
DNA601 <dnavarrete6001@gmail.com>
2022-08-18 10:57:18 -0700

    creating thought route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,14 @@
 const router = require('express').Router();
 const {
-
+    getThoughts,
+    createAThought,
+    getThoughtId,
+    reaction,
+    updateThought
 } = require('../../controllers/thoughtController')
+router.route("/").get(getThoughts).post(createAThought);
+router.route("/:id")
+    .get(getThoughtId)
+    .put(updateThought)
+router.route('/:thoughtId/reactions').post(reaction);
+module.exports = router;
